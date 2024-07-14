5275d7eb407eb4a8c9a78e74bafb4bb38b49c109
Ofir Adato <ofiradatowork@gmail.com>
2024-07-14 18:03:45 +0300

    Update conftest.py

--- page_test/conftest.py
+++ page_test/conftest.py
@@ -11,8 +11,8 @@ def driverd(request):
         "user-data-dir=C:\\Users\\linoy\\AppData\\Local\\Google\\Chrome\\User Data\\profile-directory=Profile 5")
     service = ChromeService(executable_path="D:\\Python\\Python311\\Scripts\\chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=options)
-    driver.set_page_load_timeout(30)  # Set page load timeout
-    driver.set_script_timeout(30)  # Set script timeout
+    driver.set_page_load_timeout(30) 
+    driver.set_script_timeout(30)  
     request.cls.driver = driver
     driver.maximize_window()
     yield driver
